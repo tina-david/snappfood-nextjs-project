9ea87bbe541129dadaf857c5c73c5e7ae6933bb1
Tina-david <tinadavid838@gmail.com>
2024-12-18 21:32:03 +0330

    feat(swiper): Add swiper in to the project and add swiper in slide component

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,9 @@
+'use client'
 import {DATABASE} from "@/src/data/data";
 import Image from "next/image";
 import {MdArrowBackIosNew, MdOutlineStar} from "react-icons/md";
+import {Swiper, SwiperSlide, useSwiper} from "swiper/react";
+import 'swiper/css';
 
 const Categories = () => {
     return (
@@ -28,8 +31,19 @@ const Categories = () => {
     )
 }
 
+const SwiperNextButton = () => {
+    const swiper = useSwiper();
+    return (
+        <div
+            className={'rounded-full z-50 p-4 bg-white cursor-pointer shadow-shadows-medium hover:shadow-shadows-high absolute top-36 -left-4'}
+            onClick={() => swiper.slideNext()}>
+            <MdArrowBackIosNew style={{color: '#ff00a6'}}/>
+        </div>
+    )
+}
+
 const Slide = () => {
-    return(
+    return (
         <div className={'flex max-w-7xl flex-wrap w-full'}>
             <div className={'flex justify-between w-full my-4'}>
                 <div className={'text-xl font-bold'}>
@@ -40,43 +54,50 @@ const Slide = () => {
                     <MdArrowBackIosNew style={{marginRight: '8px', marginTop: '4px'}}/>
                 </div>
             </div>
-            <div className={'flex w-full flex-wrap'}>
-                {DATABASE.restaurants.map( restaurant => (
-                    <div className={'flex flex-wrap w-1/4 p-1'} key={restaurant.id}>
-                        <div className={'flex flex-wrap w-full'}>
-                            <div className={'w-full bg-amber-600 h-36 rounded-t-xl relative'}>
-                                <Image src={restaurant.image} alt={restaurant.name} width={100} height={100}
-                                       style={{width: '100%', height: '100%', borderRadius: '8px 8px 0 0'}}/>
+            <div className={'flex w-full flex-wrap relative'}>
+                <Swiper spaceBetween={20} slidesPerView={4}>
+                    <SwiperNextButton/>
+                    {DATABASE.restaurants.map(restaurant => (
+                        <SwiperSlide key={restaurant.id}>
+                            <div className={'flex flex-wrap w-full p-1'}>
                                 <div
-                                    className={'absolute bg-white top-8 rounded-tl-xl rounded-bl-xl px-4 py-1 text-secondary-main text-sm'}>
-                                    {restaurant.pay}
+                                    className={'flex flex-wrap w-full cursor-pointer shadow-shadows-medium hover:shadow-shadows-high'}>
+                                    <div className={'w-full bg-amber-600 h-36 rounded-t-xl relative'}>
+                                        <Image src={restaurant.image} alt={restaurant.name} width={100} height={100}
+                                               style={{width: '100%', height: '100%', borderRadius: '8px 8px 0 0'}}/>
+                                        <div
+                                            className={'absolute bg-white top-8 rounded-tl-xl rounded-bl-xl px-4 py-1 text-secondary-main text-sm'}>
+                                            {restaurant.pay}
+                                        </div>
+                                    </div>
+                                    <div className={'flex flex-col w-full justify-center items-center -mt-8 z-40'}>
+                                        <div className={'w-20 h-20 rounded-xl border-white border-solid border-4'}>
+                                            <Image src={restaurant.logo} alt={restaurant.name} width={100} height={100}
+                                                   style={{width: '100%', height: '100%', borderRadius: '8px'}}/>
+                                        </div>
+                                        <div className={'mt-4 text-lg font-bold'}>
+                                            {restaurant.name}
+                                        </div>
+                                        <div className={'flex my-1 text-sm'}>
+                                            <MdOutlineStar style={{color: '#FACB0F', marginLeft: '2px'}}/>
+                                            {` ${restaurant.star} `}
+                                            {` (${restaurant.rate}) `}
+                                        </div>
+                                        <div className={'my-1 text-sm text-gray5-color'}>
+                                            {restaurant.otherCats.map(cat => cat)}
+                                        </div>
+                                        <div className={'flex text-xs my-4'}>
+                                            <Image src={restaurant.delivery.icon} alt={''} width={16} height={16}
+                                                   style={{marginLeft: '4px'}}/>
+                                            {` ${restaurant.delivery.type} `}
+                                            {` ${restaurant.delivery.price}  تومان`}
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
-                            <div className={'flex flex-col w-full justify-center items-center -mt-8 z-40'}>
-                                <div className={'w-20 h-20 rounded-xl border-white border-solid border-4'}>
-                                    <Image src={restaurant.logo} alt={restaurant.name} width={100} height={100}
-                                           style={{width: '100%', height: '100%', borderRadius: '8px'}}/>
-                                </div>
-                                <div className={'mt-4 text-lg font-bold'}>
-                                    {restaurant.name}
-                                </div>
-                                <div className={'flex my-1 text-sm'}>
-                                    <MdOutlineStar style={{color: '#FACB0F', marginLeft: '2px'}}/>
-                                    {` ${restaurant.star} `}
-                                    {` (${restaurant.rate}) `}
-                                </div>
-                                <div className={'my-1 text-sm text-gray5-color'}>
-                                    {restaurant.otherCats.map( cat => cat)}
-                                </div>
-                                <div className={'flex text-xs mt-4 mb-1'}>
-                                    <Image src={restaurant.delivery.icon} alt={''} width={16} height={16} style={{marginLeft: '4px'}}/>
-                                    {` ${restaurant.delivery.type} `}
-                                    {` ${restaurant.delivery.price}  تومان`}
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                ))}
+                        </SwiperSlide>
+                    ))}
+                </Swiper>
             </div>
         </div>
     )
