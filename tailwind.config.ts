02f44c4654464f5f780a3073cc64ef04808d3b6d
Tina-david <tinadavid838@gmail.com>
2024-12-24 18:54:36 +0330

    feat(page): Fix show empty products and history

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,9 +2,12 @@ import type {Config} from "tailwindcss";
 
 export default {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './app/**/*.{js,ts,jsx,tsx,mdx}', // Note the addition of the `app` directory.
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+
+    // Or if using `src` directory:
+    './src/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
