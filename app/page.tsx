dea7c890a698ab78b2c11ea2d72bdc5df8b95844
Tina-david <tinadavid838@gmail.com>
2025-03-07 14:15:57 +0330

    contact-us

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,9 @@
 'use client'
-
 import Categories from "@/src/component/Categories";
 import Slide from "@/src/component/Slide";
 import ContactUs from "@/src/component/ContactUs";
 
 export default function Home() {
-
     return (
         <div className={'flex w-full justify-center flex-wrap p-4'}>
             <Categories/>
